fac916280cac6519b68d7991e793e34fdc71191f
MBJ <thiswallz@gmail.com>
2022-09-03 15:26:19 -0400

    base

--- test-case-1.spec.js
+++ test-case-1.spec.js
@@ -5,28 +5,46 @@ const puppeteer = require('puppeteer')
 let browser 
 
 beforeAll(async () => {
-  browser = await puppeteer.launch();
-})
+  browser = await puppeteer.launch({
+    timeout: 90000,
+    headless: true,
+    args: ['--no-sandbox', "--disable-setuid-sandbox", "--disable-gpu"]
+  });
+}, 100000)
 
 expect.extend({ toMatchImageSnapshot });
 
 describe('sum module',  () => {
-  test('Print 0001', async () => {
-    const page = await browser.newPage();
+  let page;
+
+  beforeEach(async () => {
+    page = await browser.newPage();
     await page.goto('http://localhost:3000', { waitUntil: "domcontentloaded" });
     await page.waitForNavigation();
-    const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot({
-      customSnapshotIdentifier: 'local_name',
-    });
+  const data = await page.content()
+  console.log('data', data)
+  })
+  test('Print 0001', async () => {
+    try{
+      const image = await page.screenshot();
+      expect(image).toMatchImageSnapshot({
+        customSnapshotIdentifier: 'local_name1',
+      });
+    }catch(err){
+      console.log('screenshot', err)
+    }
+
   });
   test('Print 0002', async () => {
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000', { waitUntil: "domcontentloaded" });
-    await page.waitForNavigation();
     const image = await page.screenshot();
     expect(image).toMatchImageSnapshot({
-      customSnapshotIdentifier: 'local_name',
+      customSnapshotIdentifier: 'local_name2',
+    });
+  });
+  test('Print 0003', async () => {
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot({
+      customSnapshotIdentifier: 'local_name3',
     });
   });
 });
